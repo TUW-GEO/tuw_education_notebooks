557efb39d9fe1626bc5ac1c9b2f244e3046bfb9a
MartinSchobben <schobbenmartin@gmail.com>
2024-05-29 08:49:19 +0200

    add plot title

--- src/tuw_education_notebooks/view_seasonal_ssm.py
+++ src/tuw_education_notebooks/view_seasonal_ssm.py
@@ -1,6 +1,6 @@
 import matplotlib
 import xarray as xr
-
+import numpy as np
 from pathlib import Path
 from eomaps import Maps
 from matplotlib.colors import LinearSegmentedColormap
@@ -25,19 +25,20 @@ def view_seasonal_ssm(quarterly_datacube, cmap):
         j.add_feature.preset.coastline()
         j.add_feature.preset.ocean()
         j.add_feature.preset.countries()
-        # j.add_feature.preset.land(hatch="////")
+
+        dt = np.datetime_as_string(quarterly_datacube.isel(time=i).time.to_numpy(), unit='D')
+        j.add_title(dt)
         j.set_data(quarterly_datacube.isel(time=i), x="x", y="y", parameter="band_data", crs=Maps.CRS.Equi7_EU)
         j.plot_map(cmap=cmap, zorder=3)
+        j.add_colorbar(label="surface soil moisture (1)")
 
-    m.add_colorbar(label="surface soil moisture (1)", pos=(0.1, 0.1, 0.8, 0.1))
-
-    m.apply_layout(
-        {
-            "figsize": [6.4, 4.8],
-            "0_map": [0.1, 0.6, 0.5, 0.5],
-            "1_map": [0.6, 0.6, 0.5, 0.5],
-            "2_map": [0.1, 0.1, 0.5, 0.5],
-            "3_map": [0.6, 0.1, 0.5, 0.5],
-        }
-    )
+    # m.apply_layout(
+    #     {
+    #         "figsize": [6.4, 4.8],
+    #         "0_map": [0.1, 0.6, 0.5, 0.4],
+    #         "1_map": [0.6, 0.6, 0.5, 0.4],
+    #         "2_map": [0.1, 0.1, 0.5, 0.4],
+    #         "3_map": [0.6, 0.1, 0.5, 0.4],
+    #     }
+    # )
     m.show()
